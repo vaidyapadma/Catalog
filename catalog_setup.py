74ec90674ba21bd8eaecbf92ade87004ef32304b
Padmakshi Vaidya <padmakshi.vaidya@gmail.com>
2019-02-26 16:58:16 +0530

    Update catalog_setup.py

--- catalog_setup.py
+++ catalog_setup.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column , ForeignKey , Integer , String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship , sessionmaker , joinedload ,backref
+from sqlalchemy.orm import relationship , sessionmaker , joinedload , backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
